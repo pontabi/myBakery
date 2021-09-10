4a792d8b377a85adfcbe517322bd904204a417d9
pontabi <51449117+pontabi@users.noreply.github.com>
2021-09-10 13:57:37 +0900

    Add files via upload
    mvをスライドショーにして画像が順番に切り替わるようにした。
    
    [ 修正が必要な点 ]
    画像が拡大されて表示されてしまう。

--- js/main.js
+++ js/main.js
@@ -11,4 +11,18 @@
   close.addEventListener('click', () => {
     overlay.classList.remove('show');
   });
+
+  let count = 0;
+  function slideShow() {
+    const images = [
+      './img/mv-background0.jpg',
+      './img/mv-background1.jpg',
+      './img/mv-background2.jpg',
+    ];
+    document.getElementById('mv').style.background = `url(${images[count % images.length]})`;
+    count++;
+    // const randomIndex = Math.floor(Math.random() * images.length);
+    setTimeout(slideShow, 3000);
+  }
+  slideShow();
 }
